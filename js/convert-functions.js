5a23c5113b8456ece23213bcfb56e716cb9cba97
philatm <philamooh@gmail.com>
2021-10-05 18:03:53 +0500

    v-0.0.4 Convertion between cm, mm, and m are correct

--- js/convert-functions.js
+++ js/convert-functions.js
@@ -2,3 +2,13 @@ const mmToM = mm => 0.001 * mm;
 const cmToM = cm => 0.01 * cm;
 const mToMm = m => 1000 * m;
 const mToCm = m => 100 * m;
+const toM = {
+	'mm': mmToM,
+	'cm': cmToM,
+	'm': (x) => x,
+};
+const fromM = {
+	'mm': mToMm,
+	'cm': mToCm,
+	'm': (x) => x,
+}
