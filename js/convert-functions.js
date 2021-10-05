40dddafa8c97748d9c97210fb12532b905bf37aa
philatm <philamooh@gmail.com>
2021-10-05 18:13:54 +0500

    v-0.1.0 Convertion from input is working well

--- js/convert-functions.js
+++ js/convert-functions.js
@@ -1,14 +1,24 @@
 const mmToM = mm => 0.001 * mm;
 const cmToM = cm => 0.01 * cm;
+const mileToM = mile => 1609.344 * mile;
+const footToM = foot => 0.3048 * foot;
+
 const mToMm = m => 1000 * m;
 const mToCm = m => 100 * m;
+const mToMile = m => m / 1609.344;
+const mToFoot = m => m / 0.3048;
+
 const toM = {
 	'mm': mmToM,
 	'cm': cmToM,
 	'm': (x) => x,
+	'mile': mileToM,
+	'foot': footToM,
 };
 const fromM = {
 	'mm': mToMm,
 	'cm': mToCm,
 	'm': (x) => x,
+	'mile': mToMile,
+	'foot': mToFoot,
 }
